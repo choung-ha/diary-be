4704cf78299d8c99873b02d85f4651d760b42bc4
kochungcheon <g2c1010@naver.com>
2025-02-23 17:01:11 +0900

    feat: ReactiveMongoTemplate 추가 및 논블로킹 MongoDB 접근 설정 업데이트

---
+++ diary-llm/src/main/java/com/chungha/diaryllm/DiaryLlmApplication.java
@@ -0,0 +1,13 @@
+package com.chungha.diaryllm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryLlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryLlmApplication.class, args);
+	}
+
+}
