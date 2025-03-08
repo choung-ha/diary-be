1227d50fb86d1b68c711201d0b792f31ed9c03b9
haeun <bhaeun1105@gmail.com>
2025-03-08 15:20:08 +0900

    style: check-style 반영

--- diary-common/src/main/java/chungha/diarycommon/model/Emotion.java
+++ diary-common/src/main/java/chungha/diarycommon/model/Emotion.java
@@ -1,24 +1,24 @@
 package chungha.diarycommon.model;
 
+import java.util.stream.Stream;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
-import java.util.stream.Stream;
-
 public enum Emotion {
-    HAPPY, SAD, ANGRY, EXCITED, NEUTRAL;
+	HAPPY, SAD, ANGRY, EXCITED, NEUTRAL;
 
-    @JsonCreator
-    public static Emotion fromString(String value) {
-        return Stream.of(Emotion.values())
-                .filter(e -> e.name().equalsIgnoreCase(value))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("Invalid emotion: " + value));
-    }
+	@JsonCreator
+	public static Emotion fromString(String value) {
+		return Stream.of(Emotion.values())
+			.filter(e -> e.name().equalsIgnoreCase(value))
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException("Invalid emotion: " + value));
+	}
 
-    @JsonValue
-    public String toJson() {
-        return name().toLowerCase();
-    }
+	@JsonValue
+	public String toJson() {
+		return name().toLowerCase();
+	}
 }
 
