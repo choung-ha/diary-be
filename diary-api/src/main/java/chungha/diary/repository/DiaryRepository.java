9c2a7690d721a152a9dae73001dd9e131c096339
haeun <bhaeun1105@gmail.com>
2025-03-08 15:15:26 +0900

    feat: 다이어리 API 구현
    - 다이어리 수정
    - 다이어리 삭제
    - 다이어리 페이지네이션

--- diary-api/src/main/java/chungha/diary/repository/DiaryRepository.java
+++ diary-api/src/main/java/chungha/diary/repository/DiaryRepository.java
@@ -1,26 +1,59 @@
 package chungha.diary.repository;
 
+import chungha.diary.model.request.DiaryUpdateReq;
 import chungha.diarycommon.entity.Diary;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
 @Repository
 public class DiaryRepository {
     private final MongoTemplate mongoTemplate;
 
-    public void insertDiary(Diary diary) {
-        mongoTemplate.insert(diary);
+    public void saveDiary(Diary diary) {
+        mongoTemplate.save(diary);
     }
 
-    public List<Diary> getAllDiary() {
-        return mongoTemplate.findAll(Diary.class);
+    public Page<Diary> getAllDiary(Pageable pageable) {
+        Query query = new Query().with(pageable);
+        List<Diary> diaryList = mongoTemplate.find(query, Diary.class);
+        return PageableExecutionUtils.getPage(diaryList, pageable, () -> mongoTemplate.count(query, Diary.class));
     }
 
-    public Diary getDiaryById(String diaryId) {
-        return mongoTemplate.findById(diaryId, Diary.class);
+    public Optional<Diary> getDiaryById(String diaryId) {
+        return Optional.ofNullable(mongoTemplate.findById(diaryId, Diary.class));
     }
+
+    public Diary updateDiary(DiaryUpdateReq req) {
+        Query query = new Query(Criteria.where("_id").is(req.diaryId()));
+        Update update = new Update();
+
+        if (req.title() != null && !req.title().isEmpty()) {
+            update.set("title", req.title());
+        }
+        if (req.content() != null && !req.content().isEmpty()) {
+            update.set("content", req.content());
+        }
+        if (req.emotion() != null) {
+            update.set("emotion", req.emotion());
+        }
+
+        FindAndModifyOptions options = new FindAndModifyOptions().returnNew(true); // 업데이트 후 객체 반환
+        return mongoTemplate.findAndModify(query, update, options, Diary.class);
+    }
+
+    public void deleteDiaryById(String diaryId) {
+        mongoTemplate.remove(new Query(Criteria.where("_id").is(diaryId)), Diary.class);
+    }
+
 }
--- diary-api/src/main/java/chungha/diary/service/DiaryService.java
+++ diary-api/src/main/java/chungha/diary/service/DiaryService.java
@@ -1,14 +1,19 @@
 package chungha.diary.service;
 
-import chungha.diary.model.DiaryRequest;
+import chungha.diary.model.request.DiaryCreateReq;
+import chungha.diary.model.request.DiaryDeleteReq;
+import chungha.diary.model.request.DiaryUpdateReq;
 import chungha.diary.repository.DiaryRepository;
 import chungha.diarycommon.entity.Diary;
+import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,23 +22,34 @@ public class DiaryService {
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
     private final DiaryRepository diaryRepository;
 
-    public void createDiary(DiaryRequest req) {
+    public void createDiary(DiaryCreateReq req) {
         Diary diary = Diary.builder()
                 .title(req.title())
                 .content(req.content())
                 .emotion(req.emotion())
                 .userId(req.userId())
+                .feedback(new ArrayList<>())
                 .build();
-
-        logger.info(diary.toString());
-        diaryRepository.insertDiary(diary);
+        diaryRepository.saveDiary(diary);
     }
 
-    public List<Diary> getAllDiary() {
-        return diaryRepository.getAllDiary();
+    public Page<Diary> getAllDiary(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page - 1, pageSize, Sort.by(Sort.Direction.DESC, "created_at"));
+
+        return diaryRepository.getAllDiary(pageable);
     }
 
     public Diary getDiaryById(String diaryId) {
-        return diaryRepository.getDiaryById(diaryId);
+        return diaryRepository.getDiaryById(diaryId).orElseThrow();
     }
+
+    public Diary updateDiary(DiaryUpdateReq req) {
+        return diaryRepository.updateDiary(req);
+    }
+
+    public void deleteDiary(DiaryDeleteReq req) {
+        diaryRepository.deleteDiaryById(req.diaryId());
+    }
+
+    // 유효성 검사는 서비스 로직에서
 }
