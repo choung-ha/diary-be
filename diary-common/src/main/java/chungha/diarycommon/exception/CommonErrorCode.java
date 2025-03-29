db03cb53644285a0b8ee7f308d0b62a125a74701
kochungcheon <g2c1010@naver.com>
2025-03-29 19:15:24 +0900

    feat : Error Code 정의

--- diary-common/src/main/java/chungha/diarycommon/exception/CommonErrorCode.java
+++ diary-common/src/main/java/chungha/diarycommon/exception/CommonErrorCode.java
@@ -12,8 +12,13 @@ public enum CommonErrorCode implements BaseErrorCode {
 	DIARY_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 일기입니다."),
 
 	// 유효성
-	WRONG_EMOTION(HttpStatus.BAD_REQUEST, "유효한 감정을 선택해주세요.");
+	WRONG_EMOTION(HttpStatus.BAD_REQUEST, "유효한 감정을 선택해주세요."),
 
+	// llm module
+	DIARY_FEEDBACK_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 피드백이 반영된 일기입니다."),
+	LLM_CALL_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "LLM 호출에 실패했습니다."),
+	DIARY_CONDITION_NOT_MATCH(HttpStatus.BAD_REQUEST, "조건에 맞는 일기가 없습니다."),
+	FEEDBACK_SAVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "피드백 저장에 실패했습니다.");
 	private final HttpStatus httpStatus;
 	private final String message;
 
