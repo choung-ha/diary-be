92dbab96ca81ed455c8288fa9fd08e5e9a89fb15
haeun <bhaeun1105@gmail.com>
2025-04-05 21:52:14 +0900

    chore: 인덱스 스크립트 추가

--- diary_index.js
+++ diary_index.js
@@ -1,6 +1,6 @@
 // mongosh < diary_index.js
 // 사용할 컬렉션 선택 (예: diary 컬렉션)
-use diary; // ← 실제 DB 이름으로 변경
+use diary // ← 실제 DB 이름으로 변경
 
 // 1. 복합 인덱스: userid + context(text) + title(text)
 db.diary.createIndex(
